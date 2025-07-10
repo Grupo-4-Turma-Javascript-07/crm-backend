4b20223231446cfb6ec8900727d204439402a301
Larissa Pinheiro <rissapins@gmail.com>
2025-07-10 12:01:05 -0300

    feat: cria CRUD de Categoria e relaciona com Produto

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Produto } from '../../produto/entities/produto.entity';
+
+@Entity({ name: 'tb_categorias' })
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty({ message: 'O nome da categoria é obrigatório.' })
+  @IsString({ message: 'O nome da categoria deve ser um texto.' })
+  @MinLength(3, { message: 'O nome da categoria deve ter pelo menos 3 caracteres.' })
+  @MaxLength(100, { message: 'O nome da categoria ultrapassou o limite permitido.' })
+  @Column({ length: 100, nullable: false })
+  nome: string;
+
+  @OneToMany(() => Produto, produto => produto.categoria)
+  produtos: Produto[];
+}
