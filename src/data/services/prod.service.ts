0183bc00841b2792d8afeab5af2118e9b059450e
Igor Pardinho <pardinhoigor@gmail.com>
2025-07-12 03:34:43 -0300

    feat: configurações para fazer o deploy

---
+++ src/data/services/prod.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Usuario } from '../../usuario/entities/usuario.entity';
+import { Produto } from '../../produto/entities/produto.entity';
+import { Categoria } from '../../categoria/entities/categoria.entity';
+
+@Injectable()
+export class ProdService implements TypeOrmOptionsFactory {
+  constructor(private config: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    const isProduction = this.config.get('NODE_ENV') === 'production';
+
+    return {
+      type: this.config.get<'postgres' | 'mysql'>('DB_TYPE') ?? 'postgres',
+      host: this.config.get<string>('DB_HOST') ?? 'localhost',
+      port: parseInt(this.config.get('DB_PORT') ?? '5432', 10),
+      username: this.config.get<string>('DB_USERNAME') ?? 'postgres',
+      password: this.config.get<string>('DB_PASSWORD') ?? '',
+      database: this.config.get<string>('DB_NAME') ?? 'postgres',
+      entities: [Usuario, Produto, Categoria],
+      synchronize: true,
+
+      ssl: isProduction,
+      extra: isProduction
+        ? {
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+    };
+  }
+}
