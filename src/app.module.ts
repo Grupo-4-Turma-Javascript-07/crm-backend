4f9f3e1cde5713c93c8c7dafc6b1d8b45d592870
Igor Pardinho <pardinhoigor@gmail.com>
2025-08-13 17:44:03 -0300

    configura o app module para deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,37 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
-import { CategoriaModule } from './categoria/categoria.module';
-import { ProdutoModule } from './produto/produto.module';
-import { UsuarioModule } from './usuario/usuario.module';
-import { ProdService } from './data/services/prod.service';
-import { DevService } from './data/services/dev.services';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+
     TypeOrmModule.forRootAsync({
-      useClass: ProdService,
       imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const isProd = config.get<string>('NODE_ENV') === 'production';
+
+        return isProd
+          ? {
+              type: 'postgres',
+              url: config.get<string>('DATABASE_URL'),
+              ssl: { rejectUnauthorized: false },
+              autoLoadEntities: true,
+              synchronize: true, // cuidado em prod
+            }
+          : {
+              type: config.get<'postgres' | 'mysql'>('DB_TYPE'),
+              host: config.get<string>('DB_HOST'),
+              port: config.get<number>('DB_PORT'),
+              username: config.get<string>('DB_USERNAME'),
+              password: config.get<string>('DB_PASSWORD'),
+              database: config.get<string>('DB_NAME'),
+              autoLoadEntities: true,
+              synchronize: true,
+            };
+      },
     }),
-    UsuarioModule,
-    ProdutoModule,
-    CategoriaModule,
-    AuthModule,
   ],
 })
 export class AppModule {}
